05004728d1ecc3836383d2fa04964da959ead7ea
Hestaine <ariffhestaine@gmail.com>
2024-03-13 11:34:08 +0100

    yooo

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const menuBtn = document.querySelector(".menu-open");
 const menuClose = document.querySelector(".menu-close");
 const nav = document.querySelector(".menu");
 const contacBtn = document.querySelector(".contact-btn");
+const supportBtn = document.querySelectorAll(".support-btn");
+const supportSection = document.getElementById("support");
 const contacSection = document.getElementById("contact");
 
 // Web functions
@@ -21,7 +23,17 @@ const closeMenu = function () {
 // Event listeners
 menuBtn.addEventListener("click", openMenu);
 menuClose.addEventListener("click", closeMenu);
+
+// Contact scroll
 contacBtn.addEventListener("click", (e) => {
   e.preventDefault();
   contacSection.scrollIntoView({ behavior: "smooth" });
+});
+
+// Support button scroll
+supportBtn.forEach((support) => {
+  support.addEventListener("click", (e) => {
+    e.preventDefault();
+    supportSection.scrollIntoView({ behavior: "smooth" });
+  });
 });
